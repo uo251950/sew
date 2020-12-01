692e4abcf6364be181ef366457702f6866f40453
uo251950 <71430148+uo251950@users.noreply.github.com>
2020-12-01 23:40:21 +0100

    Add files via upload

---
+++ practica3/Ejercicio-13/Tarea-2/MapaGeoJSON.js
@@ -0,0 +1,15 @@
+class Mapa {
+    initMap() {
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 11,
+            center: { lat: 41.876, lng: -87.624 },
+        });
+
+        const ctaLayer = new google.maps.KmlLayer({
+            url: "https://uo251950.github.io/sew/practica3/rutas.geojson",
+            map: map,
+        });
+    }
+}
+
+var mapa = new Mapa();
