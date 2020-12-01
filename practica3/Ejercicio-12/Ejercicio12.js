156ddf1a3eefa4a0637ec62dfd7600c6f0744482
uo251950 <71430148+uo251950@users.noreply.github.com>
2020-12-01 21:36:57 +0100

    Add files via upload

---
+++ practica3/Ejercicio-12/Ejercicio12.js
@@ -0,0 +1,63 @@
+class Comprobacion {
+    static isNavegadorValido() {
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            document.write("<p>Este navegador soporta el API File </p>");
+            return true;
+        }
+        else {
+            document.write("<p>¡¡¡ Este navegador NO soporta el API File y este programa puede no funcionar correctamente !!!</p>");
+            return false;
+        }
+    }
+}
+
+class File {
+    realizaAccion(archivos) {
+        var archivo = archivos[0];
+        var nombre = document.getElementById("nombreArchivo");
+        var tamaño = document.getElementById("tamañoArchivo");
+        var tipo = document.getElementById("tipoArchivo");
+        var ultima = document.getElementById("ultimaModificacion");
+        var contenido = document.getElementById("contenidoArchivo");
+        var areaVisualizacion = document.getElementById("areaTexto");
+        var errorArchivo = document.getElementById("errorLectura");
+        nombre.innerText = "Nombre del archivo: " + archivo.name;
+        tamaño.innerText = "Tamaño del archivo: " + archivo.size + " bytes";
+        tipo.innerText = "Tipo del archivo: " + archivo.type;
+        ultima.innerText = "Fecha de la última modificación: " + archivo.lastModifiedDate;
+        contenido.innerText="Contenido del archivo de texto:"
+        var tipoTexto = /text.plain/;
+        var tipoJson = /text.xml/;
+        var tipoXml = /application.json/;
+
+        if (archivo.type.match(tipoTexto) || archivo.type.match(tipoXml) || archivo.type.match(tipoJson))
+        {
+            var lector = new FileReader();
+            lector.onload = function (evento) {
+                //El evento "onload" se lleva a cabo cada vez que se completa con éxito una operación de lectura
+                //La propiedad "result" es donde se almacena el contenido del archivo
+                //Esta propiedad solamente es válida cuando se termina la operación de lectura
+                areaVisualizacion.innerText = lector.result;
+            }
+            lector.readAsText(archivo);
+        }
+        else {
+            errorArchivo.innerText = "Error : ¡¡¡ Archivo no válido !!!";
+        }
+
+        document.getElementById("label").remove();
+        document.getElementById("archivoTexto").remove();
+    }
+}
+
+var file = null;
+
+if (Comprobacion.isNavegadorValido()) {
+    function ejecutar(archivos) {
+        if(file == null)
+            file = new File();
+
+        file.realizaAccion(archivos);
+    }
+}
+
